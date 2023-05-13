371bd162a12c0c58a74c8a49c786b29a91a1395f
Miguel <jemiguelrivas@gmail.com>
2023-05-12 17:12:43 -0700

    fixing deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 echo "Getting ready for deployment"
-# git push git@github.com:a11y.git --delete gh-pages
+git push git@github.com:alirebleco.git --delete gh-pages
 npm run build
-# cd dist || exit
-# git init
-# git add -A
-# git commit -m 'deploy'
-# git push -f git@github.com:a11y.git master:gh-pages
+cd dist || exit
+echo "1"
+git init
+echo "2"
+git add -A
+echo "3"
+git commit -m 'deploy'
+echo "4"
+git push -f git@github.com:alirebleco.git master:gh-pages
+echo "5"
