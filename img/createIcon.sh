e03a00e41ed4066e0c99d858c85b84072800d082
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-10 21:28:25 -0700

    adding icons

---
+++ img/createIcon.sh
@@ -0,0 +1,8 @@
+sizes=(16 24 32 48 64 128 180 256)
+files=("lomdb-icon")
+
+for f in ${files[@]}; do
+  for s in ${sizes[@]}; do
+    convert ${f}.svg -resize ${s}x${s} ${f}_${s}x${s}.png
+  done
+done
