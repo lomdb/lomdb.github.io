413f6ab5375f63d68f10a33af10c870bccbfca5e
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-11 14:57:57 -0700

    adding tw players

--- js/db_users-tw.js
+++ js/db_users-tw.js
@@ -1,6 +1,312 @@
 import { user } from './classes.js'
 
 export const tw = [
+  user(['康康兒 (Kāng Kāng ér)'], 'TW_1499', 'D0202', [
+    { rank: 'top', position: 1, date: '2025-05-11' },
+  ]),
+  user(['集八卡哇 (Jí Bā Kǎ Wā)'], 'TW_1391', '7040F', [
+    { rank: 'top', position: 2, date: '2025-05-11' },
+  ]),
+  user(['強制被死亡 (Qiáng Zhì Bèi Sǐ Wáng)'], 'TW_1445', '80A0E', [
+    { rank: 'top', position: 3, date: '2025-05-11' },
+  ]),
+  user(['棒棒棒棒貓 (Bàng Bàng Bàng Bàng Māo)'], 'TW_1223', '0170A', [
+    { rank: 'top', position: 4, date: '2025-05-11' },
+  ]),
+  user(['坏坏 (Huài Huài)'], 'TW_1025', '70108', [
+    { rank: 'top', position: 5, date: '2025-05-11' },
+  ]),
+  user(['兔兔、 (Tù Tù)'], 'TW_1158', '70207', [
+    { rank: 'top', position: 6, date: '2025-05-11' },
+  ]),
+  user(['插騷鴨 (Chā Sāo Yā)'], 'TW_1084', '70C02', [
+    { rank: 'top', position: 7, date: '2025-05-11' },
+  ]),
+  user(['正正經經鴨 (Zhèng Zhèng Jīng Jīng Yā)'], 'TW_1084', 'B030F', [
+    { rank: 'top', position: 8, date: '2025-05-11' },
+  ]),
+  user(['Karidee'], 'TW_1084', 'A0B04', [
+    { rank: 'top', position: 9, date: '2025-05-11' },
+  ]),
+  user(['晴儿 (Qíng ér)'], 'TW_1328', '40E03', [
+    { rank: 'top', position: 10, date: '2025-05-11' },
+  ]),
+  user(['猴猴、 (Hóu Hóu)'], 'TW_1158', '01308', [
+    { rank: 'top', position: 11, date: '2025-05-11' },
+  ]),
+  user(['伯爵紅茶 (Bó Jué Hóng Chá)'], 'TW_1362', '40E0C', [
+    { rank: 'top', position: 12, date: '2025-05-11' },
+  ]),
+  user(['菇單北半球 (Gū Dān Běi Bàn Qiú)'], 'TW_1445', 'F070A', [
+    { rank: 'top', position: 13, date: '2025-05-11' },
+  ]),
+  user(['光明頂小學徒 (Guāng Míng Dǐng Xiǎo Xué Tú)'], 'TW_1254', 'F0902', [
+    { rank: 'top', position: 14, date: '2025-05-11' },
+  ]),
+  user(['平平安安鴨 (Píng Píng Ān Ān Yā)'], 'TW_1084', '90505', [
+    { rank: 'top', position: 15, date: '2025-05-11' },
+  ]),
+  user(['該用戶不存在 (Gāi Yòng Hù Bù Cún Zài)'], 'TW_1176', '40C05', [
+    { rank: 'top', position: 16, date: '2025-05-11' },
+  ]),
+  user(['鳥森林 (Niǎo Sēn Lín)'], 'TW_1019', 'B0C0C', [
+    { rank: 'top', position: 17, date: '2025-05-11' },
+  ]),
+  user(['玩玩 (Wán Wán)'], 'TW_1514', '80D0F', [
+    { rank: 'top', position: 18, date: '2025-05-11' },
+  ]),
+  user(['光明頂渠王 (Guāng Míng Dǐng Qú Wáng)'], 'TW_1254', 'C0105', [
+    { rank: 'top', position: 19, date: '2025-05-11' },
+  ]),
+  user(['菇哇 (Gū Wā)'], 'TW_1395', '40D02', [
+    { rank: 'top', position: 20, date: '2025-05-11' },
+  ]),
+  user(['靈靈發 (Líng Líng Fā)'], 'TW_1005', '90A0B', [
+    { rank: 'top', position: 21, date: '2025-05-11' },
+  ]),
+  user(['机甲战斗大雪王 (Jī Jiǎ Zhàn Dòu Dà Xuě Wáng)'], 'TW_1011', '50305', [
+    { rank: 'top', position: 22, date: '2025-05-11' },
+  ]),
+  user(['毛菇 (Máo Gū)'], 'TW_1362', 'A0001', [
+    { rank: 'top', position: 23, date: '2025-05-11' },
+  ]),
+  user(['光明頂蝦蝦 (Guāng Míng Dǐng Xiā Xiā)'], 'TW_1254', '70A00', [
+    { rank: 'top', position: 24, date: '2025-05-11' },
+  ]),
+  user(['Lucifer'], 'TW_1328', 'B0500', [
+    { rank: 'top', position: 25, date: '2025-05-11' },
+  ]),
+  user(['菇哇 (Gū Wā)'], 'TW_1362', '9020C', [
+    { rank: 'top', position: 26, date: '2025-05-11' },
+  ]),
+  user(['拿貢丸丟死你 (Ná Gòng Wán Diū Sǐ Nǐ)'], 'TW_1176', '80304', [
+    { rank: 'top', position: 27, date: '2025-05-11' },
+  ]),
+  user(['Y哈 (Y Hā)'], 'TW_1181', 'A0C01', [
+    { rank: 'top', position: 28, date: '2025-05-11' },
+  ]),
+  user(['Han'], 'TW_1001', 'B040A', [
+    { rank: 'top', position: 29, date: '2025-05-11' },
+  ]),
+  user(['飛羽 (Fēi Yǔ)'], 'TW_1255', 'B0B0A', [
+    { rank: 'top', position: 30, date: '2025-05-11' },
+  ]),
+  user(['荒北 (Huāng Běi)'], 'TW_1160', 'E0B0E', [
+    { rank: 'top', position: 31, date: '2025-05-11' },
+  ]),
+  user(['光明頂小學弟 (Guāng Míng Dǐng Xiǎo Xué Dì)'], 'TW_1254', '40504', [
+    { rank: 'top', position: 32, date: '2025-05-11' },
+  ]),
+  user(['菇勇者6040B (Gū Yǒng Zhě 6040B)'], 'TW_1391', '6040B', [
+    { rank: 'top', position: 33, date: '2025-05-11' },
+  ]),
+  user(['棒棒棒夫人 (Bàng Bàng Bàng Fū Rén)'], 'TW_1223', '70104', [
+    { rank: 'top', position: 34, date: '2025-05-11' },
+  ]),
+  user(['不要欺負寶寶 (Bù Yào Qī Fù Bǎo Bǎo)'], 'TW_1581', '01303', [
+    { rank: 'top', position: 35, date: '2025-05-11' },
+  ]),
+  user(['GG'], 'TW_1255', '6060B', [
+    { rank: 'top', position: 36, date: '2025-05-11' },
+  ]),
+  user(['YCK'], 'TW_1362', 'C0400', [
+    { rank: 'top', position: 37, date: '2025-05-11' },
+  ]),
+  user(['尬的偉緯 (Gà De Wěi Wěi)'], 'TW_1085', '40301', [
+    { rank: 'top', position: 38, date: '2025-05-11' },
+  ]),
+  user(['棒棒克里斯 (Bàng Bàng Kè Lǐ Sī)'], 'TW_1223', 'C0D0D', [
+    { rank: 'top', position: 39, date: '2025-05-11' },
+  ]),
+  user(['你瞅啥呢 (Nǐ Chǒu Shá Ne)'], 'TW_1181', 'D000D', [
+    { rank: 'top', position: 40, date: '2025-05-11' },
+  ]),
+  user(['小阿噗 (Xiǎo Ā Pū)'], 'TW_1084', '60303', [
+    { rank: 'top', position: 41, date: '2025-05-11' },
+  ]),
+  user(['茶茶 (Chá Chá)'], 'TW_1391', 'A0102', [
+    { rank: 'top', position: 42, date: '2025-05-11' },
+  ]),
+  user(['不冷、 (Bù Lěng)'], 'TW_1254', '70F0E', [
+    { rank: 'top', position: 43, date: '2025-05-11' },
+  ]),
+  user(['我進去了鴨 (Wǒ Jìn Qù Le Yā)'], 'TW_1084', '41702', [
+    { rank: 'top', position: 44, date: '2025-05-11' },
+  ]),
+  user(['晴儿 (Qíng ér)'], 'TW_1328', '50B0A', [
+    { rank: 'top', position: 45, date: '2025-05-11' },
+  ]),
+  user(['偷芯小狐狸 (Tōu Xīn Xiǎo Hú Lí)'], 'TW_1255', 'A0B0A', [
+    { rank: 'top', position: 46, date: '2025-05-11' },
+  ]),
+  user(['拔毛鴨 (Bá Máo Yā)'], 'TW_1084', 'A0C0C', [
+    { rank: 'top', position: 47, date: '2025-05-11' },
+  ]),
+  user(['這菇菇有毒 (Zhè Gū Gū Yǒu Dú)'], 'TW_1288', 'F0C00', [
+    { rank: 'top', position: 48, date: '2025-05-11' },
+  ]),
+  user(['菇家寡人鴨 (Gū Jiā Guǎ Rén Yā)'], 'TW_1084', '80309', [
+    { rank: 'top', position: 49, date: '2025-05-11' },
+  ]),
+  user(['晴兒 (Qíng ér)'], 'TW_1328', '90A01', [
+    { rank: 'top', position: 50, date: '2025-05-11' },
+  ]),
+  user(['蓉蓉1609 (Róng Róng 1609)'], 'TW_1001', '9010E', [
+    { rank: 'top', position: 51, date: '2025-05-11' },
+  ]),
+  user(['歐歐歐潼潼 (Ōu Ōu Ōu Tóng Tóng)'], 'TW_1255', 'D0406', [
+    { rank: 'top', position: 52, date: '2025-05-11' },
+  ]),
+  user(['光明頂化緣de (Guāng Míng Dǐng Huà Yuán de)'], 'TW_1254', 'E0402', [
+    { rank: 'top', position: 53, date: '2025-05-11' },
+  ]),
+  user(['鬍鬚 (Hú Xū)'], 'TW_1160', '9030D', [
+    { rank: 'top', position: 54, date: '2025-05-11' },
+  ]),
+  user(['超緊張 (Chāo Jǐn Zhāng)'], 'TW_1019', 'D0A09', [
+    { rank: 'top', position: 55, date: '2025-05-11' },
+  ]),
+  user(['瞇瞇傲嬌小公主 (Mī Mī Ào Jiāo Xiǎo Gōng Zhǔ)'], 'TW_1019', '30E0F', [
+    { rank: 'top', position: 56, date: '2025-05-11' },
+  ]),
+  user(['光明頂欠超渡 (Guāng Míng Dǐng Qiàn Chāo Dù)'], 'TW_1254', 'D0300', [
+    { rank: 'top', position: 57, date: '2025-05-11' },
+  ]),
+  user(['暗黒の水滴魚 (An Hei no Shuǐ Dī Yú)'], 'TW_1255', '31002', [
+    { rank: 'top', position: 58, date: '2025-05-11' },
+  ]),
+  user(['劍劍菇 (Jiàn Jiàn Gū)'], 'TW_1470', '3110C', [
+    { rank: 'top', position: 59, date: '2025-05-11' },
+  ]),
+  user(['肥強 (Féi Qiáng)'], 'TW_1362', '40609', [
+    { rank: 'top', position: 60, date: '2025-05-11' },
+  ]),
+  user(['光明頂濕兄 (Guāng Míng Dǐng Shī Xiōng)'], 'TW_1254', '60A0C', [
+    { rank: 'top', position: 61, date: '2025-05-11' },
+  ]),
+  user(['瞇瞇傲嬌小公主 (Mī Mī Ào Jiāo Xiǎo Gōng Zhǔ)'], 'TW_1019', 'B0D08', [
+    { rank: 'top', position: 62, date: '2025-05-11' },
+  ]),
+  user(['憤怒之罪 (Fèn Nù Zhī Zuì)'], 'TW_1181', '70C00', [
+    { rank: 'top', position: 63, date: '2025-05-11' },
+  ]),
+  user(
+    ['春江水暖鴨先知 (Chūn Jiāng Shuǐ Nuǎn Yā Xiān Zhī)'],
+    'TW_1084',
+    'B0A03',
+    [{ rank: 'top', position: 64, date: '2025-05-11' }]
+  ),
+  user(['無雇主 (Wú Gù Zhǔ)'], 'TW_1452', 'D0606', [
+    { rank: 'top', position: 65, date: '2025-05-11' },
+  ]),
+  user(['韓吉 (Hán Jí)'], 'TW_1102', 'F0A09', [
+    { rank: 'top', position: 66, date: '2025-05-11' },
+  ]),
+  user(['尬的饅頭 (Gà De Mán Tóu)'], 'TW_1085', 'B0A0B', [
+    { rank: 'top', position: 67, date: '2025-05-11' },
+  ]),
+  user(['晴儿ゞ (Qíng ér)'], 'TW_1328', 'D0D01', [
+    { rank: 'top', position: 68, date: '2025-05-11' },
+  ]),
+  user(['光明頂濕妹 (Guāng Míng Dǐng Shī Mèi)'], 'TW_1254', '40709', [
+    { rank: 'top', position: 69, date: '2025-05-11' },
+  ]),
+  user(['無醉 (Wú Zuì)'], 'TW_1254', '60F03', [
+    { rank: 'top', position: 70, date: '2025-05-11' },
+  ]),
+  user(['小b (Xiǎo b)'], 'TW_1079', '4090A', [
+    { rank: 'top', position: 71, date: '2025-05-11' },
+  ]),
+  user(['眯眯傲嬌小公主 (Mī Mī Ào Jiāo Xiǎo Gōng Zhǔ)'], 'TW_1019', 'C0F05', [
+    { rank: 'top', position: 72, date: '2025-05-11' },
+  ]),
+  user(['靜靜的看 (Jìng Jìng De Kàn)'], 'TW_1288', 'F020A', [
+    { rank: 'top', position: 73, date: '2025-05-11' },
+  ]),
+  user(['爆氣小公主 (Bào Qì Xiǎo Gōng Zhǔ)'], 'TW_1025', '11503', [
+    { rank: 'top', position: 74, date: '2025-05-11' },
+  ]),
+  user(['給我回溯還原劑 (Gěi Wǒ Huí Sù Huán Yuán Jì)'], 'TW_1328', '70D05', [
+    { rank: 'top', position: 75, date: '2025-05-11' },
+  ]),
+  user(['焦糖冰奶茶 (Jiāo Táng Bīng Nǎi Chá)'], 'TW_1391', '50000', [
+    { rank: 'top', position: 76, date: '2025-05-11' },
+  ]),
+  user(['龍龍、 (Lóng Lóng)'], 'TW_1158', 'E0F0A', [
+    { rank: 'top', position: 77, date: '2025-05-11' },
+  ]),
+  user(['閒對子 (Xián Duì Zi)'], 'TW_1362', 'A0900', [
+    { rank: 'top', position: 78, date: '2025-05-11' },
+  ]),
+  user(['晴儿々 (Qíng ér)'], 'TW_1328', '70604', [
+    { rank: 'top', position: 79, date: '2025-05-11' },
+  ]),
+  user(['紅茶 (Hóng Chá)'], 'TW_1212', 'B0901', [
+    { rank: 'top', position: 80, date: '2025-05-11' },
+  ]),
+  user(['咕嚕嚕 (Gū Lū Lū)'], 'TW_1001', '41708', [
+    { rank: 'top', position: 81, date: '2025-05-11' },
+  ]),
+  user(['湯湯、 (Tāng Tāng)'], 'TW_1158', '70207', [
+    { rank: 'top', position: 82, date: '2025-05-11' },
+  ]),
+  user(['克利迪斯 (Kè Lì Dí Sī)'], 'TW_1001', '9000B', [
+    { rank: 'top', position: 83, date: '2025-05-11' },
+  ]),
+  user(['無情 (Wú Qíng)'], 'TW_1321', 'C050B', [
+    { rank: 'top', position: 84, date: '2025-05-11' },
+  ]),
+  user(['小天天 (Xiǎo Tiān Tiān)'], 'TW_1019', 'F0E0A', [
+    { rank: 'top', position: 85, date: '2025-05-11' },
+  ]),
+  user(['黑貓貓貓 (Hēi Māo Māo Māo)'], 'TW_1543', '51F0F', [
+    { rank: 'top', position: 86, date: '2025-05-11' },
+  ]),
+  user(['臭臭 (Chòu Chòu)'], 'TW_1581', 'B0A01', [
+    { rank: 'top', position: 87, date: '2025-05-11' },
+  ]),
+  user(['小麻吉 (Xiǎo Má Jí)'], 'TW_1411', '60D09', [
+    { rank: 'top', position: 88, date: '2025-05-11' },
+  ]),
+  user(['有種過來呀 (Yǒu Zhǒng Guò Lái Ya)'], 'TW_1102', '80807', [
+    { rank: 'top', position: 89, date: '2025-05-11' },
+  ]),
+  user(['空虛寂寞覺得冷 (Kōng Xū Jì Mò Jué Dé Lěng)'], 'TW_1212', 'E0600', [
+    { rank: 'top', position: 90, date: '2025-05-11' },
+  ]),
+  user(['荒西 (Huāng Xī)'], 'TW_1160', 'C050C', [
+    { rank: 'top', position: 91, date: '2025-05-11' },
+  ]),
+  user(['肥嘟嘟伊衛門 (Féi Dū Dū Yī Wèi Mén)'], 'TW_1511', 'A1606', [
+    { rank: 'top', position: 92, date: '2025-05-11' },
+  ]),
+  user(['惡貝比 (È Bèi Bǐ)'], 'TW_1395', '50D03', [
+    { rank: 'top', position: 93, date: '2025-05-11' },
+  ]),
+  user(['六姨母 (Liù Yí Mǔ)'], 'TW_1160', 'E050A', [
+    { rank: 'top', position: 94, date: '2025-05-11' },
+  ]),
+  user(['邊緣喵子 (Biān Yuán Miāo Zi)'], 'TW_1102', '40806', [
+    { rank: 'top', position: 95, date: '2025-05-11' },
+  ]),
+  user(['棒棒棒武器 (Bàng Bàng Bàng Wǔ Qì)'], 'TW_1223', 'E020F', [
+    { rank: 'top', position: 96, date: '2025-05-11' },
+  ]),
+  user(['光明頂吃瓜的 (Guāng Míng Dǐng Chī Guā De)'], 'TW_1254', '90209', [
+    { rank: 'top', position: 97, date: '2025-05-11' },
+  ]),
+  user(['菜市場口阿勇 (Cài Shì Chǎng Kǒu Ā Yǒng)'], 'TW_1499', '40700', [
+    { rank: 'top', position: 98, date: '2025-05-11' },
+  ]),
+  user(['咩修敢某 (Miē Xiū Gǎn Mǒu)'], 'TW_1485', '0150A', [
+    { rank: 'top', position: 99, date: '2025-05-11' },
+  ]),
+  user(
+    ['曾是愛說謊的女人 (Céng Shì Ài Shuō Huǎng De Nǚ Rén)'],
+    'TW_1561',
+    'C0607',
+    [{ rank: 'top', position: 100, date: '2025-05-11' }]
+  ),
   //------------------------------- scouts
   user(['Pavel'], 'TW_1817', '31802', [
     { rank: 'detective', date: '2025-04-25' },
