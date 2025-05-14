5aa74af81cd4249ba9c2997940efcb5e737b2420
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-13 20:07:35 -0700

    gaps section refactor

--- js/lom_gaps.js
+++ js/lom_gaps.js
@@ -1,10 +1,8 @@
 import './modules/index.js'
 import { spreadServers } from './db_merges.js'
 import { getPrefix, createNode } from './helpers.js'
-import { getCountryCode } from './utils.js'
 import './component_users.js'
 import './component_filters.js'
-// import { countryCodes } from './enum_country-codes.js'
 import './component_navbar.js'
 
 class Gaps extends HTMLElement {
@@ -13,32 +11,20 @@ class Gaps extends HTMLElement {
   }
 
   #data = {
-    attrs: [],
     language: 'amen',
     spreadServers,
     template: `
-    <nn-caja padding="4" class="base">
-      <lom-navbar></lom-navbar>
-      <lom-filters></lom-filters>
-      <div id="all-servers" class="base"></div>
-    </nn-caja>
+      <nn-caja padding="4" class="base">
+        <lom-navbar></lom-navbar>
+        <lom-filters></lom-filters>
+        <div id="all-servers" class="base"></div>
+      </nn-caja>
     `,
   }
 
-  #servers = [
-    {
-      title: 'AMEN (1)',
-      filter: 'amen',
-      starts: 1,
-      ends: 900,
-    },
-
-    {
-      title: 'ES (6)',
-      filter: 'es',
-      starts: 1,
-      ends: 144,
-    },
+  #serverConfigs = [
+    { title: 'AMEN (1)', filter: 'amen', starts: 1, ends: 900 },
+    { title: 'ES (6)', filter: 'es', starts: 1, ends: 144 },
     {
       title: 'ESPT (11)',
       filter: 'espt',
@@ -46,18 +32,8 @@ class Gaps extends HTMLElement {
       starts: 248,
       ends: 500,
     },
-    {
-      title: 'PT (11)',
-      filter: 'pt',
-      starts: 1,
-      ends: 247,
-    },
-    {
-      title: 'EUEN (30)',
-      filter: 'euen',
-      starts: 1,
-      ends: 261,
-    },
+    { title: 'PT (11)', filter: 'pt', starts: 1, ends: 247 },
+    { title: 'EUEN (30)', filter: 'euen', starts: 1, ends: 261 },
     {
       title: 'MUSH (30)',
       filter: 'mush',
@@ -65,24 +41,9 @@ class Gaps extends HTMLElement {
       starts: 262,
       ends: 500,
     },
-    {
-      title: 'DE (33)',
-      filter: 'de',
-      starts: 1,
-      ends: 400,
-    },
-    {
-      title: 'FR (36)',
-      filter: 'fr',
-      starts: 1,
-      ends: 400,
-    },
-    {
-      title: 'ME (39)',
-      filter: 'me',
-      starts: 1,
-      ends: 90,
-    },
+    { title: 'DE (33)', filter: 'de', starts: 1, ends: 400 },
+    { title: 'FR (36)', filter: 'fr', starts: 1, ends: 400 },
+    { title: 'ME (39)', filter: 'me', starts: 1, ends: 90 },
     {
       title: 'TR (39)',
       filter: 'tr',
@@ -90,86 +51,41 @@ class Gaps extends HTMLElement {
       starts: 91,
       ends: 200,
     },
-    {
-      title: 'RU (42)',
-      filter: 'ru',
-      starts: 1,
-      ends: 400,
-    },
+    { title: 'RU (42)', filter: 'ru', starts: 1, ends: 400 },
+    { title: 'CN (1)', filter: 'cn', starts: 1, ends: 18 },
+    { title: 'VN (4)', filter: 'vn', starts: 1, ends: 900 },
+    { title: 'ID (7)', filter: 'id', starts: 1, ends: 36 },
+    { title: 'EN (10)', filter: 'en', starts: 1, ends: 600 },
+    { title: 'TH (13)', filter: 'th', starts: 1, ends: 400 },
+    { title: 'KR', filter: 'kr', starts: 0, ends: 0 },
+    { title: 'JP', filter: 'jp', starts: 0, ends: 0 },
+    { title: 'TW', filter: 'tw', starts: 1001, ends: 1800 },
+  ]
 
-    {
-      title: 'CN (1)',
-      filter: 'cn',
-      starts: 1,
-      ends: 18,
-    },
-    {
-      title: 'VN (4)',
-      filter: 'vn',
-      starts: 1,
-      ends: 900,
-    },
-    {
-      title: 'ID (7)',
-      filter: 'id',
-      starts: 1,
-      ends: 36,
-    },
-    {
-      title: 'EN (10)',
-      filter: 'en',
-      starts: 1,
-      ends: 600,
-    },
-    {
-      title: 'TH (13)',
-      filter: 'th',
-      starts: 1,
-      ends: 400,
-    },
-
-    {
-      title: 'KR',
-      filter: 'kr',
-      starts: 0,
-      ends: 0,
-    },
-    {
-      title: 'JP',
-      filter: 'jp',
-      starts: 0,
-      ends: 0,
-    },
-    {
-      title: 'TW',
-      filter: 'tw',
-      starts: 1001,
-      ends: 1800,
-    },
-  ].map(e => {
-    const internalfilter = e.internalfilter || e.filter
-    const serversMerged = this.#data.spreadServers
-      .filter(s => s.split('_')[0] === internalfilter.toUpperCase())
-      .map(s => +s.split('_')[1])
-    const range = this.#generateRange(e.starts, e.ends)
-    const servers = range.map(currentServer => {
-      const found = serversMerged?.includes(currentServer)
-      return {
-        id: currentServer,
-        found,
-      }
-    })
-    return {
-      title: e.title,
-      filter: e.filter,
-      servers,
-    }
-  })
+  #servers = []
 
   #generateRange(start, end) {
-    return Array.from({ length: end - start + 1 }, (_, i) => i + start).map(
-      server => server
-    )
+    return Array.from({ length: end - start + 1 }, (_, i) => i + start)
+  }
+
+  #normalizeServers() {
+    this.#servers = this.#serverConfigs.map(config => {
+      const internal = config.internalfilter || config.filter
+      const merged = this.#data.spreadServers
+        .filter(s => s.startsWith(internal.toUpperCase() + '_'))
+        .map(s => +s.split('_')[1])
+      const mergedSet = new Set(merged)
+      const range = this.#generateRange(config.starts, config.ends)
+
+      return {
+        title: config.title,
+        filter: config.filter,
+        servers: range.map(id => ({
+          id,
+          found: mergedSet.has(id),
+        })),
+      }
+    })
   }
 
   #generateListeners() {
@@ -181,6 +97,8 @@ class Gaps extends HTMLElement {
       if (!button || !filterContainer.contains(button)) return
 
       const lang = button.classList[0]
+      if (lang === this.#data.language) return
+
       this.#data.language = lang
 
       this.querySelectorAll('.filters button').forEach(btn =>
@@ -196,15 +114,12 @@ class Gaps extends HTMLElement {
     const container = this.querySelector('#all-servers')
     container.innerHTML = ''
 
-    document
-      .querySelector('.filters button.' + this.#data.language)
-      .classList.add('active')
-
     this.#servers
-      .filter(item => {
-        if (this.#data.language === 'all') return true
-        return this.#data.language === item.filter
-      })
+      .filter(item =>
+        this.#data.language === 'all'
+          ? true
+          : item.filter === this.#data.language
+      )
       .forEach(serv => {
         createNode({
           type: 'h2',
@@ -215,9 +130,7 @@ class Gaps extends HTMLElement {
         const fila = createNode({
           type: 'nn-fila',
           parent: container,
-          attrs: {
-            class: 'table-body',
-          },
+          attrs: { class: 'table-body' },
         })
 
         serv.servers.forEach(server => {
@@ -232,10 +145,16 @@ class Gaps extends HTMLElement {
           })
         })
       })
+
+    const activeBtn = this.querySelector(
+      `.filters button.${this.#data.language}`
+    )
+    activeBtn?.classList.add('active')
   }
 
   connectedCallback() {
     this.innerHTML = this.#data.template
+    this.#normalizeServers()
     this.#generateGapsList()
     this.#generateListeners()
   }
